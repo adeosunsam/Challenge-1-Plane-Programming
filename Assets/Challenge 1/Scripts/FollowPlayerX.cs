c65af357b56dcee2286f906a5682de6f15b77589
Allos <adeosunsamuel30@gmail.com>
2023-08-30 00:57:35 +0100

    Initial commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    void Awake()
+    {
+        offset = new Vector3(25f, 0, 5f);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
